da27789b122f812213d4e83bb4127bc64082ec16
LamHongPhong1401 <21130473@st.hcmuaf.edu.vn>
2024-04-25 00:08:29 +0700

    feat: add event accordion for see detail coupon

---
+++ js/list_of_vouchers.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let toggleCollapsed = false
+    const accordionBtn = $('.accordion-button')
+    accordionBtn.click(function(){
+        if( !toggleCollapsed){
+            toggleCollapsed = true
+            $('.accordion-button').attr('data-bs-toggle', "")
+            return
+        }
+        accordionBtn.addClass('collapsed').attr('aria-expanded', false)
+        $('#collapse_coupon_desc').removeClass('show')
+        accordionBtn.attr('data-bs-toggle', "collapse")
+        toggleCollapsed = false
+    })
+})
