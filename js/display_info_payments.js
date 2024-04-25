be479c25b38be19a5cd1bb8f479a4d1499c193a7
LamHongPhong1401 <21130473@st.hcmuaf.edu.vn>
2024-04-25 21:45:03 +0700

    refactor: change style button apply coupon

--- js/display_info_payments.js
+++ js/display_info_payments.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('.vouchers').load('../component/list_of_vouchers.html');
+    for (let i = 0; i < 5; i++) {
+        $(`<div class="coupont-${i} p-3"></div>`).appendTo($('.vouchers'));
+        $(`.coupont-${i}`).load('../component/list_of_vouchers.html')
+    }
+
 })
