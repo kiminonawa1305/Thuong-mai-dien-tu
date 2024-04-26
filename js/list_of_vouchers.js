b0ab0f8c84e45d3e6f4400f9f4335f30aad9c5d7
LamHongPhong1401 <21130473@st.hcmuaf.edu.vn>
2024-04-26 22:17:39 +0700

    feat: add accordion for modal coupon list

--- js/list_of_vouchers.js
+++ js/list_of_vouchers.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     cloneAccordionItem()
     addEventAccordion()
    showAccordionActive()
+    closeAccordionMain()
 })
 
 function addEventAccordion() {
@@ -14,13 +15,20 @@ function addEventAccordion() {
             $(this).attr('data-bs-target', `#penalStayOpen-${index}`)
             $(this).attr('aria-controls', `penalStayOpen-${index}`)
         });
+        $(this).find(".apply_coupon_btn").each(function () {
+            $(this).attr('data-id', `apply-coupon-${index}`)
+        });
     });
 }
 
 function cloneAccordionItem() {
     for (let i = 0; i < 5; i++) {
         const cloneCoupon = $('.coupon-item:first').clone()
-        $('.list-coupons').append(cloneCoupon)
+        if($('.list-coupons > .coupon-item').length < 2) {
+            $('.list-coupons > .coupon-item').before(cloneCoupon)
+        }else{
+            $('#hidden-coupon > .accordion-body').append(cloneCoupon)
+        }
     }
 }
 
@@ -30,13 +38,32 @@ function showAccordionActive() {
             $(this).removeClass('active')
             $(this).addClass('collapsed')
             $(this).attr('aria-expanded', 'false')
-            $(this).parents('.accordion-item').find('.accordion-collapse').removeClass('show')
             $(this).attr('data-bs-toggle', 'collapse')
+            if($(this).attr('class').indexOf('see-more-btn') >= 0) {
+                $(this).text('Xem thêm')
+                $(this).parents('.hidden-list-coupons').find('#hidden-coupon').removeClass('show')
+                return
+            }
+            $(this).parents('.visibility-item').find('.accordion-collapse').removeClass('show')
+
         }else{
             $(this).addClass('active')
-            $(this).toggleClass('changed')
             $(this).attr('data-bs-toggle', '')
+            if($(this).attr('class').indexOf('see-more-btn') >= 0) $(this).text('Rút gọn')
         }
+        $(this).toggleClass('changed')
     })
-    return $('.accordion-button.active').map(function () {return this.className}).get()
+}
+
+function closeAccordionMain() {
+    const closeAccordion = $('.btn-close[aria-label]')
+    const seeMoreBtn = $('.see-more-btn')
+    closeAccordion.click(function(){
+        seeMoreBtn.text('Xem thêm')
+        seeMoreBtn.removeClass('active')
+        seeMoreBtn.addClass('collapsed')
+        seeMoreBtn.attr('aria-expanded', 'false')
+        seeMoreBtn.parents('.hidden-list-coupons').find('.accordion-collapse').removeClass('show')
+        seeMoreBtn.attr('data-bs-toggle', 'collapse')
+    })
 }
