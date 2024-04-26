f7a97c864d15163d285b101c4ab6d11468da3921
LamHongPhong1401 <21130473@st.hcmuaf.edu.vn>
2024-04-26 12:05:23 +0700

    fix: don't get text coupon id

--- js/display_info_payments.js
+++ js/display_info_payments.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+    loadCoupon()
+})
+
+let a = $('#coupon-id-0').attr(`id`)
+// console.log(a)
+function loadCoupon() {
     for (let i = 0; i < 5; i++) {
-        $(`<div class="coupont-${i} p-3"></div>`).appendTo($('.vouchers'));
+        $(`<div id="coupont-id-${i}" class="coupont-${i} p-3"></div>`).appendTo($('.vouchers'));
         $(`.coupont-${i}`).load('../component/list_of_vouchers.html')
     }
 
-})
+}
+function addIDToCoupon() {
+    for (let i = 0; i < 5; i++) {
+       // console.log($(`.coupont-${i} .accordion-item > .accordion-collapse`).attr('id'))
+        $(`.coupont-${i} .accordion-item > .accordion-collapse`).attr('id', `panelsStayOpen-${i}`)
+        $(`.coupont-${i} .accordion-button`).attr('data-bs-target','#'+ $(`.coupont-${i} .accordion-item div`).attr('id'))
+        $(`.coupont-${i} .accordion-button`).attr('aria-controls', $(`.coupont-${i} .accordion-item div`).attr('id'))
+
+    }
+}
--- js/list_of_vouchers.js
+++ js/list_of_vouchers.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
+
+})
+
+function showAccordion() {
     let toggleCollapsed = false
     const accordionBtn = $('.accordion-button')
-    accordionBtn.toggleClass('changed');
+
     accordionBtn.click(function () {
+        console.log($(this))
+        $(this).toggleClass('changed');
         if (!toggleCollapsed) {
             toggleCollapsed = true
-            $('.accordion-button').attr('data-bs-toggle', "")
+            $(this).attr('data-bs-toggle', "")
             return
         }
-        accordionBtn.addClass('collapsed').attr('aria-expanded', false)
+        $(this).addClass('collapsed').attr('aria-expanded', false)
         $('#collapse_coupon_desc').removeClass('show')
-        accordionBtn.attr('data-bs-toggle', "collapse")
+        $(this).attr('data-bs-toggle', "collapse")
 
         toggleCollapsed = false
     })
-})
+}
