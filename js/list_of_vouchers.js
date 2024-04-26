70ee753e84b44edec48c3450f6d8df3ffb19c366
LamHongPhong1401 <21130473@st.hcmuaf.edu.vn>
2024-04-26 15:14:56 +0700

    fix: complete accordion for coupon

--- js/list_of_vouchers.js
+++ js/list_of_vouchers.js
@@ -1,23 +1,42 @@
 $(document).ready(function () {
-
+    cloneAccordionItem()
+    addEventAccordion()
+   showAccordionActive()
 })
 
-function showAccordion() {
-    let toggleCollapsed = false
-    const accordionBtn = $('.accordion-button')
+function addEventAccordion() {
+    const coupons = $('.coupon-item')
+    coupons.each(function (index) {
+        $(this).find(".accordion-collapse").each(function () {
+            $(this).attr('id', `penalStayOpen-${index}`)
+        });
+        $(this).find(".accordion-button").each(function () {
+            $(this).attr('data-bs-target', `#penalStayOpen-${index}`)
+            $(this).attr('aria-controls', `penalStayOpen-${index}`)
+        });
+    });
+}
 
-    accordionBtn.click(function () {
-        console.log($(this))
-        $(this).toggleClass('changed');
-        if (!toggleCollapsed) {
-            toggleCollapsed = true
-            $(this).attr('data-bs-toggle', "")
-            return
+function cloneAccordionItem() {
+    for (let i = 0; i < 5; i++) {
+        const cloneCoupon = $('.coupon-item:first').clone()
+        $('.list-coupons').append(cloneCoupon)
+    }
+}
+
+function showAccordionActive() {
+    $('.accordion-button').click(function () {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active')
+            $(this).addClass('collapsed')
+            $(this).attr('aria-expanded', 'false')
+            $(this).parents('.accordion-item').find('.accordion-collapse').removeClass('show')
+            $(this).attr('data-bs-toggle', 'collapse')
+        }else{
+            $(this).addClass('active')
+            $(this).toggleClass('changed')
+            $(this).attr('data-bs-toggle', '')
         }
-        $(this).addClass('collapsed').attr('aria-expanded', false)
-        $('#collapse_coupon_desc').removeClass('show')
-        $(this).attr('data-bs-toggle', "collapse")
-
-        toggleCollapsed = false
     })
+    return $('.accordion-button.active').map(function () {return this.className}).get()
 }
